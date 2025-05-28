df5a7295d09ba7045ecb194c6fd61f8d85a62a89
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-05-28 15:57:08 -0300

    feat: add CoursesComponent with routing and basic template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './components/courses/courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+  { path: 'courses', loadComponent: () => import('./components/courses/courses.component').then(c => c.CoursesComponent) },
+];
