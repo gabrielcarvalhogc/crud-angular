d2e95d5c181cae677cce3cc8dcd8f6c5901414bf
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-05-28 14:42:57 -0300

    feat: integrate Angular Material and implement dark/light theme toggle
    - Added Angular Material dependencies for toolbar and slide toggle.
    - Updated app.component.html to include a toolbar with theme toggle.
    - Created app.component.scss for layout styling and theme management.
    - Modified app.component.ts to handle theme toggling logic.
    - Enhanced app.config.ts to include animations support.
    - Updated index.html to include Material Icons and Roboto font.
    - Defined custom light and dark themes in styles.scss using Angular Material theming.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatSlideToggleModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'crud-angular';
+  @HostBinding('class.dark-theme') isDark = false;
+
+  toggleTheme(checked: boolean) {
+    this.isDark = checked;
+  }
 }
