2b328c8b4eeb7bc5c1172331de154f75f69aa4f7
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-05-29 10:32:17 -0300

    feat: implement CoursesComponent with table display and styling

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  constructor() {
+    this.courses = [ {_id: '1', name: 'Angular Basics', category: 'Frontend' },];
+  }
 
+  courses: Course[];
+  displayedColumns = ['name', 'category'];
 }
