1b6009dfb769b5e9e487727326c20b24d41833d9
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-05-30 18:03:23 -0300

    feat: enhance test configuration for Jest and expand CoursesComponent tests

--- src/app/components/courses/courses.component.spec.ts
+++ src/app/components/courses/courses.component.spec.ts
@@ -20,4 +20,31 @@ describe('CoursesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render mat-card', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('mat-card')).toBeTruthy();
+  });
+
+  it('should render "Cursos disponíveis" correctly', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('span').textContent).toContain('Cursos disponíveis');
+  });
+
+  it('should render course collun headers', () => {
+    const compiled = fixture.nativeElement;
+    const headers = compiled.querySelectorAll('th');
+
+    expect(headers[0].textContent).toContain('Curso');
+    expect(headers[1].textContent).toContain('Categoria');
+  });
+
+  it('should render course rows', () => {
+    const compiled = fixture.nativeElement;
+    const rows = compiled.querySelectorAll('td');
+    const coursesMock = [ {_id: '1', name: 'Angular Basics', category: 'Frontend' },];
+
+    expect(rows[0].textContent).toContain(coursesMock[0].name);
+    expect(rows[1].textContent).toContain(coursesMock[0].category);
+  });
 });
