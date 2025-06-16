7702f23f454d031c2f83b6515cdcfe1f0b5fa1ce
Gabriel Carvalho <gabriel225gc@gmail.com>
2025-06-16 09:40:53 -0300

    feat: enhance CoursesService with additional HTTP methods and improve code structure

--- src/app/services/courses.service.spec.ts
+++ src/app/services/courses.service.spec.ts
@@ -2,18 +2,17 @@ import { TestBed } from '@angular/core/testing';
 
 import { CoursesService } from './courses.service';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { Course } from '../components/courses/model/course';
 import { of } from 'rxjs';
 
 describe('CoursesService', () => {
   let service: CoursesService;
-  let httpClientMock: { get: jest.Mock, post: jest.Mock };
-  //const API_URL = 'courses.json';
+  let httpClientMock: { get: jest.Mock, post: jest.Mock, put: jest.Mock, delete: jest.Mock };
   const API_URL = 'http://localhost:8080/api/courses';
   const cursosMock: Course[] = [
-    { _id: 1, name: 'Angular Básico', category: 'Frontend' } as Course,
-    { _id: 2, name: 'Spring Boot Avançado', category: 'Backend' } as Course
+    { _id: '1', name: 'Angular Básico', category: 'Frontend' } as Course,
+    { _id: '2', name: 'Spring Boot Avançado', category: 'Backend' } as Course
   ];
 
   beforeEach(() => {
@@ -26,7 +25,9 @@ describe('CoursesService', () => {
 
     httpClientMock = {
       get: jest.fn(),
-      post: jest.fn()
+      post: jest.fn(),
+      put: jest.fn(),
+      delete: jest.fn()
     };
 
     service = new CoursesService(httpClientMock as unknown as HttpClient);
@@ -36,24 +37,65 @@ describe('CoursesService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should call http.get with rigth URL', () => {
-    service.getList();
+  describe('should call http.get', () => {
+    it('should call http.get with rigth URL', () => {
+      service.getList();
 
-    expect(httpClientMock.get).toHaveBeenCalledWith(API_URL);
+      expect(httpClientMock.get).toHaveBeenCalledWith(API_URL);
+    });
+
+    it('should return courses list ', (done) => {
+      httpClientMock.get.mockReturnValue(of(cursosMock));
+
+      service.getList().subscribe((resultado) => {
+        expect(resultado).toEqual(cursosMock);
+        done();
+      });
+    });
+
+    it('Should return the right url id when loadById is called', () => {
+      service.loadById(1);
+      expect(httpClientMock.get).toHaveBeenCalledWith(`${API_URL}/1`);
+      service.loadById(2);
+      expect(httpClientMock.get).toHaveBeenCalledWith(`${API_URL}/2`);
+    });
   });
 
-  it('should return courses list ', (done) => {
-    httpClientMock.get.mockReturnValue(of(cursosMock));
+  describe('should call http.post', () => {
+    it('should call http.post with right URL', () => {
+      const novoCurso =  { name: 'Curso de Testes', category: 'Testes' } as Course;
+      service.save(novoCurso);
 
-    service.getList().subscribe((resultado) => {
-      expect(resultado).toEqual(cursosMock);
-      done();
+      expect(httpClientMock.post).toHaveBeenCalledWith(API_URL, novoCurso);
+    });
+
+    it('should return the created course', (done) => {
+      const novoCurso =  { name: 'Curso de Testes', category: 'Testes' } as Course;
+      httpClientMock.post.mockReturnValue(of(novoCurso));
+
+      service.save(novoCurso).subscribe((resultado) => {
+        expect(resultado).toEqual(novoCurso);
+        done();
+      });
     });
   })
 
-  it('should call the right POST Url', () => {
-    service.save(cursosMock[0]);
+  describe('should call http.put', () => {
+    it('should call http.put with right URL', () => {
+      const cursoAtualizado = { _id: '1', name: 'Curso Atualizado', category: 'Atualização' } as Course;
+      service.save(cursoAtualizado);
 
-    expect(httpClientMock.post).toHaveBeenCalledWith(API_URL, cursosMock[0]);
+      expect(httpClientMock.put).toHaveBeenCalledWith(`${API_URL}/${cursoAtualizado._id}`, cursoAtualizado);
+    });
+
+    it('should return the updated course', (done) => {
+      const cursoAtualizado = { _id: '1', name: 'Curso Atualizado', category: 'Atualização' } as Course;
+      httpClientMock.put.mockReturnValue(of(cursoAtualizado));
+
+      service.save(cursoAtualizado).subscribe((resultado) => {
+        expect(resultado).toEqual(cursoAtualizado);
+        done();
+      });
+    });
   });
 });
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course } from '../components/courses/model/course';
-import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +8,32 @@ import { first, tap } from 'rxjs';
 export class CoursesService {
 
   //private API = 'courses.json';
-  private API = 'http://localhost:8080/api/courses';
+  private readonly API = 'http://localhost:8080/api/courses';
 
   constructor(
-    private http: HttpClient,
-  ) {}
+    private readonly http: HttpClient,
+  ) { }
 
   getList() {
     return this.http.get<Course[]>(this.API)
   }
 
+  loadById(id: number) {
+    return this.http.get<Course>(`${this.API}/${id}`);
+  }
+
   save(record: Course) {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Course) {
     return this.http.post<Course>(this.API, record);
+  }
+
+  private update(record: Course) {
+    return this.http.put<Course>(`${this.API}/${record._id}`, record);
   }
 }
